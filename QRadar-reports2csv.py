96bc756ad17c77aad7abe70b4f22d2082caf202a
Nico de Smidt <QRadarGroupie@users.noreply.github.com>
2015-09-05 21:16:23 +0200

    rerelease of innitial scrip
    now with usage instructions

--- QRadar-reports2csv.py
+++ QRadar-reports2csv.py
@@ -1,5 +1,13 @@
-# python3 script
-# Nico de Smidt 2015
+#
+# QRadar python3 script to retrieve all report templates available in QRadar
+# Nico de Smidt 20150905
+#
+# run this script on a python3 enabled machine, this is typically not a QRadar appliance
+# Usage: pyhton3 QRadar-reports2csv.py [report-ContentExport-20150521204234.tar.gz] > reports.csv
+# where [report-ContentExport-20150521204234.tar.gz] is derived by running:
+# ./contentManagement.pl --action export --content-type report --id all
+# on your QRadar system, and copy the resulting .tar.gz file to your python3 machine
+#
 
 import sys, os
 import json, time
@@ -21,22 +29,15 @@ def main():
 		if "/" not in tarinfo.name and tarinfo.isreg():
 			mainfile=tarinfo.name
 	with tar.extractfile(mainfile) as myfile:
-#		print(myfile)
 		tree = ET.parse(myfile)
-#		print(tree)
 		root = tree.getroot()
-#		print(ET.dump(root))
 		for reportfile in root.findall('./report/filedata'):
-#			print(reportfile.text)
 			with tar.extractfile(reportfile.text) as mycontentfile:
 				contenttree = ET.parse(mycontentfile)
 				contentroot = contenttree.getroot()
-#				print(ET.dump(contentroot))
 				contentName= contentroot.find("*/void[@property='title']/string")
-#				print('"'+str(contentName.text)+'"')
 				contentDescription= contentroot.find("*/void[@property='descrition']/string")
 				print('"'+str(contentName.text)+'","'+str(contentDescription.text)+'"')
-
 
 if __name__ == "__main__":
     main()
